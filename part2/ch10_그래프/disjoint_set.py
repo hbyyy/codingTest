1c4010bb04a52e6b1b5e65e77ce735f3b63e379b
hbyyy <qjaduddl94@gmail.com>
2021-03-22 18:23:52 +0900

    disjoint_set(union-find)

---
+++ part2/ch10_그래프/disjoint_set.py
@@ -0,0 +1,41 @@
+# def find_parent(parent, x):
+#     if parent[x] != x:
+#         return find_parent(parent, parent[x])
+#     return x
+
+# 경로 압축 기법 사용
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, x, y):
+    _x = find_parent(parent, x)
+    _y = find_parent(parent, y)
+
+    if _x < _y:
+        parent[_y] = _x
+    else:
+        parent[_x] = _y
+
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+print('각 원소가 속한 집합 : ', end=' ')
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end=' ')
+
+print()
+
+print('부모 테이블 : ', end=' ')
+for i in range(1, v + 1):
+    print(parent[i], end=' ')
