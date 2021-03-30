737a4493f3439e47932409ad8567c2daedf4ce4d
hbyyy <qjaduddl94@gmail.com>
2021-03-30 22:38:47 +0900

    커밋 정리

--- part3/다이나믹 프로그래밍/376.py
+++ part3/다이나믹 프로그래밍/376.py
@@ -4,21 +4,15 @@ DP = []
 arr = []
 n = int(input())
 for _ in range(n):
-    arr.append(list(map(int, input().split())))
+    DP.append(list(map(int, input().split())))
 
-DP.append(arr[0])
-DP.append([arr[0][0] + arr[1][0], arr[0][0] + arr[1][1]])
-
-for i in range(2, n):
-    tmp = []
-    for j in range(len(arr[i])):
+for i in range(1, n):
+    for j in range(len(DP[i])):
         if j == 0:
-            v = DP[i - 1][j] + arr[i][j]
-        elif j == len(arr[i]) - 1:
-            v = DP[i - 1][j - 1] + arr[i][j]
+            v = DP[i - 1][j] + DP[i][j]
+        elif j == len(DP[i]) - 1:
+            v = DP[i - 1][j - 1] + DP[i][j]
         else:
-            v = max(DP[i - 1][j - 1], DP[i - 1][j]) + arr[i][j]
-        tmp.append(v)
-    DP.append(tmp.copy())
-    tmp.clear()
+            v = max(DP[i - 1][j - 1], DP[i - 1][j]) + DP[i][j]
+        DP[i][j] = v
 print(max(DP[n - 1]))
