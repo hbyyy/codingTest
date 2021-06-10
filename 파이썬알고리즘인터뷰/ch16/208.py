344fb532ae34270d4a97a5ea1e6fd3e4d2d0731d
hbyyy <qjaduddl94@gmail.com>
2021-06-10 19:14:58 +0900

    trie 구현 풀이

---
+++ 파이썬알고리즘인터뷰/ch16/208.py
@@ -0,0 +1,106 @@
+"""
+208. Trie 구현
+"""
+
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+
+        return True
+
+
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = {}
+
+
+# defaultdict 이용
+
+from collections import defaultdict
+
+
+class TrieNode2:
+    def __init__(self):
+        self.word = False
+        self.children = defaultdict(TrieNode)
+
+
+class Trie2:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for c in word:
+            node = node.children[c]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+
+        return True
